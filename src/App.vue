4f58847014215e2301c9d87127ebafea5ea7950b
Karolina Redden <dubajkaro@gmail.com>
2023-03-29 13:56:20 -0300

    add function which changes the remnder value after doubleclicking, it has been done with spread operator

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
       }
     },
     toggleReminder(id) {
-      console.log(id)
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task)
     }
   },
   created() {
