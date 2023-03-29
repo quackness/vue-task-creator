b89ad01ddc2e7f65d11e1b5e5ea9e9a38986794e
Karolina Redden <dubajkaro@gmail.com>
2023-03-28 21:13:08 -0300

    import Task component, regsiter it and bind it

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,13 +1,16 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <h3>{{task.text}}</h3>
-
+    <Task :task="task"/>
   </div>
 </template>
 
 <script>
+import Task from './Task.vue'
 export default {
   name: 'Tasks',
+  components: {
+    Task
+  },
    props: {
     tasks: Array
   },
