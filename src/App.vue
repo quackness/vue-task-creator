f3c7e0257915e94d9b8c161e6c6b6835526f37e3
Karolina Redden <dubajkaro@gmail.com>
2023-03-30 15:09:43 -0300

    add fetch single task method

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,11 @@ export default {
       const res = await fetch('http://localhost:5000/tasks')
       const data = await res.json()
       return data
-
+    },
+        async fetchTask(id) {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      return data
     }
   },
   async created() {
