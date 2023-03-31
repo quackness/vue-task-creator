45bd4c86dc43ce20a91f81b637420946396c8cd3
Karolina Redden <dubajkaro@gmail.com>
2023-03-30 21:54:07 -0300

    add request with fetch to delete a task

--- src/App.vue
+++ src/App.vue
@@ -44,9 +44,14 @@ export default {
       const data = await res.json()
       this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        //request:
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert("Error deleting task")
+        
       }
     },
     toggleReminder(id) {
