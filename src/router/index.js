725ccd95b4220aa57f81d1eaf62fa1e343381392
Karolina Redden <dubajkaro@gmail.com>
2023-03-31 12:19:12 -0300

    import About page view, create routes with the path, name and component they should link to

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-roter'
+import About from '../views/About'
+
+const routes = [
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
