7161e27b5d3d4c094c3acc928c945ef4fdb68a62
Karolina Redden <dubajkaro@gmail.com>
2023-03-31 12:57:31 -0300

    bring components from the APP to Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="showAddtask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+    <!-- //vbind tasks to tasks data  -->
+</template>
+
+<script>
+  import Tasks from "../components/Tasks";
+  import AddTask from "../components/AddTask";
+  export default {
+    name: "Home",
+    components: {
+      Tasks,
+      AddTask
+    },
+    data: {
+      tasks: []
+    }
+  }
+</script>
