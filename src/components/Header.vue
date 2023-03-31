0bd2297871449c43ad647bb4dea1a9be9343faa7
Karolina Redden <dubajkaro@gmail.com>
2023-03-31 14:00:19 -0300

    create a logic to show Add task button in the header only when a user is at the home(/) route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,35 @@
 <template>
   <header>
-    <h1>{{title}}</h1>
-    <Button 
-    @btn-click="$emit('toggle-add-task')" 
-    :text="btnTextVariation ? 'Close' : 'Add Task'" 
-    :color="btnTextVariation ? 'red' : 'green'"/>
+    <h1>{{ title }}</h1>
+    <Button v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="btnTextVariation ? 'Close' : 'Add Task'"
+      :color="btnTextVariation ? 'red' : 'green'"
+    />
   </header>
 </template>
 
 <script>
-import Button from './Button'
+import Button from "./Button";
 export default {
-  name: 'Header',
+  name: "Header",
   props: {
     title: String,
-    btnTextVariation: Boolean
+    btnTextVariation: Boolean,
   },
   components: {
-    Button
-  }
-}
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
