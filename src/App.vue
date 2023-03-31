a13eddccf6d69443670203251d25619784e9d7b3
Karolina Redden <dubajkaro@gmail.com>
2023-03-30 22:17:36 -0300

    add request to toggle the reminder

--- src/App.vue
+++ src/App.vue
@@ -54,15 +54,27 @@ export default {
         
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+     
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !data.reminder} : task)
     },
     async fetchTasks() {
       const res = await fetch('api/tasks')
       const data = await res.json()
       return data
     },
-        async fetchTask(id) {
+      async fetchTask(id) {
       const res = await fetch(`api/tasks/${id}`)
       const data = await res.json()
       return data
