4d8f7af19ef550c1d66e5085a8f936ed02aa1097
Karolina Redden <dubajkaro@gmail.com>
2023-03-31 11:50:06 -0300

    add Footer file

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+<footer>
+  <p>Copyright &copy; 2023</p>
+  <a href="/about">About</a>
+  
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+a {
+  color: #333,
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
